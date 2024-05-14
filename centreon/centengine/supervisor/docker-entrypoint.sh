2665400b21781ae7145630cd37c7ce8d74a1968c
Tpo76 <fthepaut@centreon.com>
2024-05-14 17:08:08 -0400

    esmtprc update for k8s configMap

--- centreon/centengine/supervisor/docker-entrypoint.sh
+++ centreon/centengine/supervisor/docker-entrypoint.sh
@@ -2,8 +2,6 @@
 
 set -x
 
-echo -e "mda \"procmail -d %T\"\nhostname = ${SMTP_ADDR}" > /etc/esmtprc
-
 
 rpm -ivh --nodeps centreon-engine*.rpm centreon-engine-daemon*.rpm centreon-broker-core*.rpm centreon-broker-cbmod*.rpm centreon-perl-libs*.rpm centreon-common*.rpm centreon-broker*.rpm centreon-perl-libs*.rpm
 # rm -f *.rpm
